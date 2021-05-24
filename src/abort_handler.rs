936e4dcbce07c04d7793a1f04e4246eb45e1d1fb
Roope Salmi <rpsalmi@gmail.com>
2021-05-24 11:29:13 +0300

    Use sigaction() instead of signal()

--- src/abort_handler.rs
+++ src/abort_handler.rs
@@ -1,17 +1,24 @@
-use libc::{c_int, c_void, sighandler_t, signal, SIGABRT, SIGILL};
+use libc::{c_int, c_void, sighandler_t};
 
 extern "C" fn handler(_: c_int) {
     eprintln!("Abort: Unexpected error.");
     std::process::exit(127);
 }
 
+#[allow(unused)]
 fn get_handler() -> sighandler_t {
-    handler as extern "C" fn(c_int) as *mut c_void as sighandler_t
+    handler as *mut c_void as sighandler_t
 }
 
 pub fn setup() {
+    #[cfg(unix)]
     unsafe {
-        signal(SIGABRT, get_handler());
-        signal(SIGILL, get_handler());
+        use libc::{sigaction, sigemptyset, sigset_t, SIGABRT, SIGILL};
+        use std::{mem, ptr};
+        let mut descriptor: sigaction = mem::zeroed();
+        descriptor.sa_sigaction = get_handler();
+        sigemptyset(&mut descriptor.sa_mask as *mut sigset_t);
+        sigaction(SIGABRT, &mut descriptor as *mut sigaction, ptr::null_mut());
+        sigaction(SIGILL, &mut descriptor as *mut sigaction, ptr::null_mut());
     }
 }
