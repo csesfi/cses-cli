58a25e0632708d61eed2516bae83ebd4a2f489c9
Antto Heikura <antto.heikura@gmail.com>
2021-05-25 16:03:29 +0300

    improvements

--- src/storage.rs
+++ src/storage.rs
@@ -1,60 +1,46 @@
+use anyhow::Result;
 use miniserde::{json, Deserialize, Serialize};
+use std::default::Default;
 use std::fs;
 use std::path::Path;
 
 const FILENAME: &str = "filestorage.json";
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Default, Serialize, Deserialize, Debug)]
 pub struct FileStorage {
-    username: String,
-    password: String,
-    token: String,
-    course: String,
-    task: String,
-    language: String,
-    option: String,
-    file: String,
+    username: Option<String>,
+    password: Option<String>,
+    token: Option<String>,
+    course: Option<String>,
+    task: Option<String>,
+    language: Option<String>,
+    option: Option<String>,
+    file: Option<String>,
 }
 
 impl FileStorage {
     pub fn new() -> Self {
-        let empty = FileStorage {
-            username: String::from(""),
-            password: String::from(""),
-            token: String::from(""),
-            course: String::from(""),
-            task: String::from(""),
-            language: String::from(""),
-            option: String::from(""),
-            file: String::from(""),
-        };
         if !Path::new(FILENAME).exists() {
-            return empty;
+            return Default::default();
         };
         let data = fs::read_to_string(FILENAME).unwrap();
         let res = json::from_str(&data);
         match res {
             Ok(fs) => fs,
-            Err(_) => empty,
+            Err(_) => Default::default(),
         }
     }
 }
 
-impl Default for FileStorage {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
 pub trait Storage {
-    fn get_username(&self) -> String;
-    fn get_password(&self) -> String;
-    fn get_token(&self) -> String;
-    fn get_course(&self) -> String;
-    fn get_task(&self) -> String;
-    fn get_language(&self) -> String;
-    fn get_option(&self) -> String;
-    fn get_file(&self) -> String;
+    fn get_username(&self) -> Option<&str>;
+    fn get_password(&self) -> Option<&str>;
+    fn get_token(&self) -> Option<&str>;
+    fn get_course(&self) -> Option<&str>;
+    fn get_task(&self) -> Option<&str>;
+    fn get_language(&self) -> Option<&str>;
+    fn get_option(&self) -> Option<&str>;
+    fn get_file(&self) -> Option<&str>;
     fn set_username(&mut self, val: String);
     fn set_password(&mut self, val: String);
     fn set_token(&mut self, val: String);
@@ -63,64 +49,88 @@ pub trait Storage {
     fn set_language(&mut self, val: String);
     fn set_option(&mut self, val: String);
     fn set_file(&mut self, val: String);
-    fn save(&self);
-    fn delete(&self);
+    fn save(&mut self) -> Result<()>;
+    fn delete(&mut self) -> Result<()>;
 }
 
 impl Storage for FileStorage {
-    fn get_username(&self) -> String {
-        self.username.to_string()
+    fn get_username(&self) -> Option<&str> {
+        match &self.username {
+            Some(val) => Some(val),
+            None => None,
+        }
     }
-    fn get_password(&self) -> String {
-        self.password.to_string()
+    fn get_password(&self) -> Option<&str> {
+        match &self.password {
+            Some(val) => Some(val),
+            None => None,
+        }
     }
-    fn get_token(&self) -> String {
-        self.token.to_string()
+    fn get_token(&self) -> Option<&str> {
+        match &self.token {
+            Some(val) => Some(val),
+            None => None,
+        }
     }
-    fn get_course(&self) -> String {
-        self.course.to_string()
+    fn get_course(&self) -> Option<&str> {
+        match &self.course {
+            Some(val) => Some(val),
+            None => None,
+        }
     }
-    fn get_task(&self) -> String {
-        self.task.to_string()
+    fn get_task(&self) -> Option<&str> {
+        match &self.task {
+            Some(val) => Some(val),
+            None => None,
+        }
     }
-    fn get_language(&self) -> String {
-        self.language.to_string()
+    fn get_language(&self) -> Option<&str> {
+        match &self.language {
+            Some(val) => Some(val),
+            None => None,
+        }
     }
-    fn get_option(&self) -> String {
-        self.option.to_string()
+    fn get_option(&self) -> Option<&str> {
+        match &self.option {
+            Some(val) => Some(val),
+            None => None,
+        }
     }
-    fn get_file(&self) -> String {
-        self.file.to_string()
+    fn get_file(&self) -> Option<&str> {
+        match &self.file {
+            Some(val) => Some(val),
+            None => None,
+        }
     }
     fn set_username(&mut self, val: String) {
-        self.username = val;
+        self.username = Some(val);
     }
     fn set_password(&mut self, val: String) {
-        self.password = val;
+        self.password = Some(val);
     }
     fn set_token(&mut self, val: String) {
-        self.token = val;
+        self.token = Some(val);
     }
     fn set_course(&mut self, val: String) {
-        self.course = val;
+        self.course = Some(val);
     }
     fn set_task(&mut self, val: String) {
-        self.task = val;
+        self.task = Some(val);
     }
     fn set_language(&mut self, val: String) {
-        self.language = val;
+        self.language = Some(val);
     }
     fn set_option(&mut self, val: String) {
-        self.option = val;
+        self.option = Some(val);
     }
     fn set_file(&mut self, val: String) {
-        self.file = val;
+        self.file = Some(val);
     }
-    fn save(&self) {
-        fs::write(FILENAME, json::to_string(&self)).unwrap();
+    fn save(&mut self) -> Result<()> {
+        Ok(fs::write(FILENAME, json::to_string(self))?)
     }
-    fn delete(&self) {
-        fs::remove_file(FILENAME).unwrap();
+    fn delete(&mut self) -> Result<()> {
+        Ok(fs::remove_file(FILENAME)?)
     }
 }
 
@@ -130,16 +140,7 @@ mod tests {
 
     #[test]
     fn setters_and_getters_work() {
-        let mut storage = FileStorage {
-            username: String::from(""),
-            password: String::from(""),
-            token: String::from(""),
-            course: String::from(""),
-            task: String::from(""),
-            language: String::from(""),
-            option: String::from(""),
-            file: String::from(""),
-        };
+        let mut storage: FileStorage = Default::default();
         storage.set_username(String::from("username"));
         storage.set_password(String::from("password"));
         storage.set_token(String::from("token"));
@@ -148,13 +149,13 @@ mod tests {
         storage.set_language(String::from("language"));
         storage.set_option(String::from("option"));
         storage.set_file(String::from("file"));
-        assert_eq!(String::from("username"), storage.get_username());
-        assert_eq!(String::from("password"), storage.get_password());
-        assert_eq!(String::from("token"), storage.get_token());
-        assert_eq!(String::from("course"), storage.get_course());
-        assert_eq!(String::from("task"), storage.get_task());
-        assert_eq!(String::from("language"), storage.get_language());
-        assert_eq!(String::from("option"), storage.get_option());
-        assert_eq!(String::from("file"), storage.get_file());
+        assert_eq!(String::from("username"), storage.get_username().unwrap());
+        assert_eq!(String::from("password"), storage.get_password().unwrap());
+        assert_eq!(String::from("token"), storage.get_token().unwrap());
+        assert_eq!(String::from("course"), storage.get_course().unwrap());
+        assert_eq!(String::from("task"), storage.get_task().unwrap());
+        assert_eq!(String::from("language"), storage.get_language().unwrap());
+        assert_eq!(String::from("option"), storage.get_option().unwrap());
+        assert_eq!(String::from("file"), storage.get_file().unwrap());
     }
 }
