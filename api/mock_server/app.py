fb970d6c9d99ba519727f32140f1d5964f0fdae8
Kalle Luopajärvi <kalle.luopajarvi@helsinki.fi>
2021-05-27 13:28:22 +0300

    Update mock server to match OpenAPI description

--- api/mock_server/app.py
+++ api/mock_server/app.py
@@ -1,15 +1,21 @@
 import connexion
 import werkzeug
 
+from connexion import NoContent
+
+from connexion.exceptions import ValidationError, BadRequestProblem
+from connexion.exceptions import Unauthorized
+from werkzeug.exceptions import MethodNotAllowed
+
 def login_get():
     if connexion.request.json == {"username": "kalle",
                                   "password": "kissa2"}:
         return {"X-Auth-Token": "asdf"}
-    return ({"message": "Invalid username/password"}, 401)
+    return ({"message": "Invalid username/password", "code": "invalid_credentials"}, 401)
 
 def logout_post(token_info):
     print(token_info)
-    return (204)
+    return (NoContent, 204)
 
 def apikey_auth(apikey, required_scopes=None):
     """Corresponds to the the apiKeyAuth in OpenAPI.
@@ -22,10 +28,24 @@ def apikey_auth(apikey, required_scopes=None):
     if apikey == "asdf":
         return {"this": "goes to logout function"}
 
-    raise werkzeug.exceptions.Unauthorized(
-        response = {"message": "Invalid Authorization token"})
+    # this will be overriden by the render_api_authentication_failed function
+    raise werkzeug.exceptions.Unauthorized()
+
+
+def render_invalid_query(exception):
+    return ({"message": "Invalid query format", "code": "client_error"}, 400)
+
+def render_api_authentication_failed(exception):
+    print("here")
+    return ({"message": "Invalid api key", "code": "invalid_api_key"}, 401)
+
+def render_method_not_allowed(exception):
+    return ({"message": "Invalid HTTP method", "code": "client_error"}, 405)
 
 app = connexion.App(__name__, specification_dir="../",
                     options={"swagger_ui": False})
+app.add_error_handler(BadRequestProblem, render_invalid_query)
+app.add_error_handler(Unauthorized, render_api_authentication_failed)
+app.add_error_handler(MethodNotAllowed, render_method_not_allowed)
 app.add_api("openapi.yaml")
 app.run(debug=True, host="127.0.0.1", port=4010)
