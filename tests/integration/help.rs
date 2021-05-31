bf032f23e236930cd762d34ef3a61d3eadbf91b7
Valokoodari <Valokoodari@users.noreply.github.com>
2021-05-31 09:40:29 +0300

    Integration tests for help flags.

--- tests/integration/help.rs
+++ tests/integration/help.rs
@@ -8,3 +8,39 @@ fn help_works() {
         .stdout(regex_match(r"(?i)usage"))
         .stderr(predicate::str::is_empty());
 }
+
+#[distributed_slice(TESTS)]
+fn h_flag_works() {
+    let assert = command().args(&["-h"]).assert();
+    assert
+        .success()
+        .stdout(regex_match(r"(?i)usage"))
+        .stderr(predicate::str::is_empty());
+}
+
+#[distributed_slice(TESTS)]
+fn help_flag_works() {
+    let assert = command().args(&["--help"]).assert();
+    assert
+        .success()
+        .stdout(regex_match(r"(?i)usage"))
+        .stderr(predicate::str::is_empty());
+}
+
+#[distributed_slice(TESTS)]
+fn h_flag_overrides_login_command() {
+    let assert = command().args(&["login", "-h"]).assert();
+    assert
+        .success()
+        .stdout(regex_match(r"(?i)usage"))
+        .stderr(predicate::str::is_empty());
+}
+
+#[distributed_slice(TESTS)]
+fn help_flag_overrides_submit_command() {
+    let assert = command().args(&["submit", "-h"]).assert();
+    assert
+        .success()
+        .stdout(regex_match(r"(?i)usage"))
+        .stderr(predicate::str::is_empty());
+}
