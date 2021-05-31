ce2913316abf8e99da3b1384f39cc73ce006a437
Anton Taleiko <anton.taleiko@gmail.com>
2021-05-31 16:27:50 +0300

    Added tests for overwriting current login and got rid of some copypaste code

--- tests/integration/login.rs
+++ tests/integration/login.rs
@@ -5,11 +5,7 @@ use crate::common::*;
 #[distributed_slice(TESTS)]
 fn login_is_successful_with_correct_username_and_password() {
     let assert = successful_login_attempt();
-    assert.success()
-        .stdout(regex_match(r"(?i)username: "))
-        .stdout(regex_match(r"(?i)password: "))
-        .stdout(regex_match(r"(?i)Login successful"))
-        .stderr(predicate::str::is_empty());
+    verify_successful_login_output(assert);
 }
 
 #[distributed_slice(TESTS)]
@@ -21,13 +17,33 @@ fn login_fails_with_incorrect_username() {
 #[distributed_slice(TESTS)]
 fn user_can_log_out() {
     let assert = successful_login_attempt();
-    assert.success()
-        .stdout(regex_match(r"(?i)username: "))
-        .stdout(regex_match(r"(?i)password: "))
-        .stdout(regex_match(r"(?i)Login successful"));
+    verify_successful_login_output(assert);
     let assert = logout_user();
-    assert.success()
-        .stdout(regex_match(r"(?i)Login invalidated successfully"))
+    verify_successful_login_invalidation(assert);
+}
+
+#[distributed_slice(TESTS)]
+fn application_knows_user_is_already_logged_in() {
+    let assert = successful_login_attempt();
+    verify_successful_login_output(assert);
+    let assert = command().args(&["login"]).write_stdin("no").assert();
+    assert
+        .success()
+        .stdout(regex_match(r"(?i)already logged in"))
+        .stderr(predicate::str::is_empty());
+}
+
+#[distributed_slice(TESTS)]
+fn user_can_overwrite_current_login() {
+    let assert = successful_login_attempt();
+    verify_successful_login_output(assert);
+    let assert = command()
+        .args(&["login"])
+        .write_stdin("yes\nkalle\nkissa2\n")
+        .assert();
+    assert
+        .success()
+        .stdout(regex_match(r"(?i)login successful"))
         .stderr(predicate::str::is_empty());
 }
 
@@ -38,6 +54,15 @@ fn successful_login_attempt() -> Assert {
         .assert()
 }
 
+fn verify_successful_login_output(assert: Assert) {
+    assert
+        .success()
+        .stdout(regex_match(r"(?i)username: "))
+        .stdout(regex_match(r"(?i)password: "))
+        .stdout(regex_match(r"(?i)login successful"))
+        .stderr(predicate::str::is_empty());
+}
+
 fn unsuccessful_login() -> Assert {
     command()
         .args(&["login"])
@@ -46,7 +71,12 @@ fn unsuccessful_login() -> Assert {
 }
 
 fn logout_user() -> Assert {
-    command()
-        .args(&["logout"])
-        .assert()
+    command().args(&["logout"]).assert()
+}
+
+fn verify_successful_login_invalidation(assert: Assert) {
+    assert
+        .success()
+        .stdout(regex_match(r"(?i)login invalidated successfully"))
+        .stderr(predicate::str::is_empty());
 }
