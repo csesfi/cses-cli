c3f2ad7e952ebaf984f497e1adf35122d819af0f
Roope Salmi <rpsalmi@gmail.com>
2021-05-31 13:12:06 +0300

    Implement service::submit

--- src/service/mod.rs
+++ src/service/mod.rs
@@ -1,48 +1,18 @@
-use crate::entities::SubmissionInfo;
-use crate::{CsesApi, Resources, Storage, RP};
+mod login;
+pub use login::{login, logout, Login};
+
+mod submit;
+pub use submit::{submission_info, submit};
+
+use crate::{Resources, Storage, RP};
 use anyhow::{anyhow, Result};
-use miniserde::{Deserialize, Serialize};
-#[derive(Serialize, Deserialize)]
-pub struct Login {
-    pub username: String,
-    pub password: String,
-}
 
 pub fn ping(_res: &mut Resources<impl RP>) -> bool {
     true
 }
 
-pub fn login(res: &mut Resources<impl RP>, login: &Login) -> Result<()> {
-    let token = res.api.login(login)?;
-    res.storage.set_token(token);
-    res.storage.save()
-}
-
-pub fn logout(res: &mut Resources<impl RP>) -> Result<()> {
-    if let Some(token) = res.storage.get_token() {
-        res.api.logout(token)?;
-        res.storage.delete()?;
-        Ok(())
-    } else {
-        Err(anyhow!("not currently logged in"))
-    }
-}
-
-pub fn submit() -> Result<u64> {
-    let submission_id = 0;
-    Ok(submission_id)
-}
-
-pub fn submission(
-    res: &mut Resources<impl RP>,
-    submission_id: u64,
-    poll: bool,
-) -> Result<SubmissionInfo> {
-    let token = res.storage.get_token().unwrap();
-    let course_id = res.storage.get_course().unwrap();
-    let task_id = res.storage.get_task().unwrap().parse::<u64>().unwrap();
-    let submission = res
-        .api
-        .get_submit(token, course_id, task_id, submission_id, poll);
-    Ok(submission?)
+fn require_login(res: &mut Resources<impl RP>) -> Result<&str> {
+    res.storage
+        .get_token()
+        .ok_or(anyhow!("Not currently logged in"))
 }
---
+++ src/service/submit.rs
@@ -0,0 +1,76 @@
+use super::require_login;
+use crate::command;
+use crate::{
+    api::CodeSubmit,
+    entities::{Language, SubmissionInfo},
+};
+use crate::{CsesApi, Filesystem, Resources, Storage, RP};
+use anyhow::{anyhow, Result};
+
+pub fn update_submit_parameters(
+    res: &mut Resources<impl RP>,
+    parameters: &command::Submit,
+) -> Result<()> {
+    if let Some(course_id) = parameters.course_id {
+        res.storage.set_course(course_id.clone());
+    }
+    if let Some(task_id) = parameters.task_id {
+        res.storage.set_task(task_id);
+    }
+    if let Some(language_name) = parameters.language_name {
+        res.storage.set_language(language_name.clone());
+    }
+    if let Some(language_option) = parameters.language_option {
+        res.storage.set_option(language_option.clone());
+    }
+    Ok(())
+}
+
+pub fn submit(res: &mut Resources<impl RP>, filename: String) -> Result<u64> {
+    require_login(res)?;
+    let course_id = res
+        .storage
+        .get_course()
+        .ok_or(anyhow!("Course not provided"))?
+        .to_owned();
+    let task_id = res
+        .storage
+        .get_task()
+        .ok_or(anyhow!("Task not provided"))?
+        .to_owned();
+    let language_name = res
+        .storage
+        .get_token()
+        .ok_or(anyhow!("Language not provided"))?
+        .to_owned();
+    let language_option = res.storage.get_token().map(|t| t.to_owned());
+
+    let content = res.filesystem.get_file(&filename)?;
+    let content = res.filesystem.encode_base64(&content);
+    let submission = CodeSubmit {
+        language: Language {
+            name: language_name,
+            option: language_option,
+        },
+        filename: filename.clone(),
+        content,
+    };
+    let submission_id =
+        res.api
+            .submit_task(require_login(res)?, &course_id, task_id, &submission)?;
+    Ok(submission_id)
+}
+
+pub fn submission_info(
+    res: &mut Resources<impl RP>,
+    submission_id: u64,
+    poll: bool,
+) -> Result<SubmissionInfo> {
+    let token = res.storage.get_token().unwrap();
+    let course_id = res.storage.get_course().unwrap();
+    let task_id = res.storage.get_task().unwrap();
+    let submission = res
+        .api
+        .get_submit(token, course_id, task_id, submission_id, poll);
+    Ok(submission?)
+}
