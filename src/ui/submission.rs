cb7322d316dceac67fd233628c3575907b053005
Antto Heikura <antto.heikura@gmail.com>
2021-06-02 13:58:25 +0300

    Improve print_submission_info +add task and sender to SubmissionInfo

--- src/ui/submission.rs
+++ src/ui/submission.rs
@@ -13,29 +13,39 @@ pub fn print_submission_info(
 ) -> Result<()> {
     let mut submission_info = service::submission_info(&mut ui.res, submission_id, long_poll)?;
     /*
+    let task = String::from("Weird Algorithm");
+    let sender = String::from("username");
     let time = String::from("2021-05-31 10:32:41");
     let language = Language {
-        name: String::from("RUST"),
-        option: Some(String::from("c++17")),
+        name: String::from("C++"),
+        option: Some(String::from("11")),
     };
     let status = String::from("PENDING");
-    let pending = true;
-    let result = Some(String::from("ACCEPTED"));
+    let pending = false;
+    let result = Some(String::from("TIME LIMIT EXCEEDED"));
     let test1 = SubmissionTestInfo {
-        number: 1,
+        number: 9,
         verdict: String::from("ACCEPTED"),
-        time: 1,
+        time: Some(1),
     };
     let test2 = SubmissionTestInfo {
-        number: 2,
+        number: 99,
         verdict: String::from("WRONG ANSWER"),
-        time: 2,
+        time: Some(1),
+    };
+    let test3 = SubmissionTestInfo {
+        number: 999,
+        verdict: String::from("TIME LIMIT EXCEEDED"),
+        time: None,
     };
     let mut tests = Vec::new();
     tests.push(test1);
     tests.push(test2);
+    tests.push(test3);
     let tests = Some(tests);
     let mut submission_info = SubmissionInfo {
+        task,
+        sender,
         time,
         language,
         status,
@@ -44,17 +54,20 @@ pub fn print_submission_info(
         tests,
     };
     */
+
+    ui.term.write_line("Submission details\n")?;
+    ui.term
+        .write_line(&format!("Task: {}", submission_info.task))?;
+    ui.term
+        .write_line(&format!("Sender: {}", submission_info.sender))?;
     ui.term
         .write_line(&format!("Submission time: {}", submission_info.time))?;
-    match submission_info.language.option {
-        Some(option) => ui.term.write_line(&format!(
-            "Language: {} {}",
-            submission_info.language.name, option
-        ))?,
-        None => ui
-            .term
-            .write_line(&format!("Language: {}", submission_info.language.name))?,
+    ui.term
+        .write_str(&format!("Language: {}", submission_info.language.name))?;
+    if let Some(option) = submission_info.language.option {
+        ui.term.write_str(&option)?;
     };
+    ui.term.write_line("")?;
     ui.term
         .write_line(&format!("Status: {}", submission_info.status))?;
     while submission_info.pending {
@@ -63,22 +76,28 @@ pub fn print_submission_info(
         ui.term
             .write_line(&format!("Status: {}", submission_info.status))?;
     }
-    if submission_info.status == "READY" {
-        ui.term.write_line(&format!(
-            "Result: {}",
-            with_color(submission_info.result.unwrap())
-        ))?;
-        ui.term.write_line("# | verdict | time (ms)")?;
-        ui.term.write_line("---------------------------")?;
-        for test in submission_info.tests.unwrap() {
-            ui.term.write_line(&format!(
-                "{} | {} | {}",
-                test.number,
-                with_color(test.verdict),
-                test.time
-            ))?;
-        }
+    if let Some(result) = submission_info.result {
+        ui.term
+            .write_line(&format!("Result: {}", with_color(result)))?;
     };
+    if let Some(tests) = submission_info.tests {
+        ui.term.write_line("Test results\n")?;
+        ui.term.write_line("test |       verdict        | time")?;
+        ui.term.write_line("------------------------------------")?;
+        for test in tests {
+            ui.term.write_str(&format!(
+                "#{:<3} | {:<20} | ",
+                test.number,
+                with_color(test.verdict)
+            ))?;
+            match test.time {
+                Some(time) => ui
+                    .term
+                    .write_line(&format!("{:.2} s", time as f64 / 1000.0))?,
+                None => ui.term.write_line("--")?,
+            };
+        }
+    }
     Ok(())
 }
 
