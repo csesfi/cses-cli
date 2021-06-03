971161218e626c3a47875cd00e2fc932a9d1c15f
Roope Salmi <rpsalmi@gmail.com>
2021-06-03 12:47:43 +0300

    Use write! and writeln!

--- src/ui/submission.rs
+++ src/ui/submission.rs
@@ -2,6 +2,7 @@ use crate::service;
 use crate::RP;
 use anyhow::Result;
 use console::{Style, StyledObject};
+use std::io::Write;
 
 use super::Ui;
 //use crate::entities::{Language, SubmissionInfo, SubmissionTestInfo};
@@ -13,46 +14,40 @@ pub fn print_submission_info(
 ) -> Result<()> {
     let mut submission_info = service::submission_info(&mut ui.res, submission_id, long_poll)?;
     ui.term.write_line("Submission details\n")?;
-    ui.term
-        .write_line(&format!("Submission time: {}", submission_info.time))?;
-    ui.term
-        .write_str(&format!("Language: {}", submission_info.language.name))?;
+    writeln!(ui.term, "Submission time: {}", submission_info.time)?;
+    write!(ui.term, "Language: {}", submission_info.language.name)?;
     if let Some(option) = submission_info.language.option {
         ui.term.write_str(&option)?;
     };
     ui.term.write_line("")?;
-    ui.term
-        .write_line(&format!("Status: {}", submission_info.status))?;
+    writeln!(ui.term, "Status: {}", submission_info.status)?;
     while submission_info.pending {
         submission_info = service::submission_info(&mut ui.res, submission_id, long_poll)?;
         ui.term.clear_last_lines(1)?;
-        ui.term
-            .write_line(&format!("Status: {}", submission_info.status))?;
+        writeln!(ui.term, "Status: {}", submission_info.status)?;
     }
 
     if let Some(compiler_report) = &submission_info.compiler {
-        ui.term
-            .write_line(&format!("\nCompiler report:\n{}", compiler_report))?;
+        writeln!(ui.term, "\nCompiler report:\n{}", compiler_report)?;
     }
 
     if let Some(result) = submission_info.result {
-        ui.term
-            .write_line(&format!("Result: {}", with_color(result)))?;
+        writeln!(ui.term, "Result: {}", with_color(result))?;
     };
     if let Some(tests) = submission_info.tests {
         ui.term.write_line("\nTest results\n")?;
         ui.term.write_line("  # |        verdict        | time")?;
-        ui.term.write_line("-------------------------------------")?;
+        ui.term
+            .write_line("-------------------------------------")?;
         for test in tests {
-            ui.term.write_str(&format!(
+            write!(
+                ui.term,
                 "{:>3} | {:<21} | ",
                 test.number,
                 with_color(test.verdict)
-            ))?;
+            )?;
             match test.time {
-                Some(time) => ui
-                    .term
-                    .write_line(&format!("{:.2} s", time as f64 / 1000.0))?,
+                Some(time) => writeln!(ui.term, "{:.2} s", time as f64 / 1000.0)?,
                 None => ui.term.write_line("--")?,
             };
         }
