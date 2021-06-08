4f7ea19620eac5321b1e949dd9670bef56747fcc
Kalle Luopajärvi <kalle.luopajarvi@helsinki.fi>
2021-06-08 17:27:34 +0300

    Add integration tests

--- tests/integration/submit.rs
+++ tests/integration/submit.rs
@@ -100,7 +100,7 @@ fn shows_each_test_result() {
 }
 
 #[distributed_slice(TESTS)]
-fn remembers_course_and_language() {
+fn remembers_course() {
     log_in("uolevi");
 
     create_file("13.rs", RS_13_CONTENT);
@@ -113,10 +113,41 @@ fn remembers_course_and_language() {
 
     create_file("main.cpp", MAIN_CPP_CONTENT);
     command()
-        .args(&["submit", "-t", "42", "main.cpp"])
+        .args(&["submit", "-t", "42", "-l", "C++", "-o", "C++17", "main.cpp"])
         .assert()
         .success()
         .stdout(regex_match(r"(?i)status.*ready"));
+}
+#[distributed_slice(TESTS)]
+fn does_not_remember_language_or_option() {
+    log_in("uolevi");
+
+    create_file("13.rs", RS_13_CONTENT);
+    command()
+        .args(&[
+            "submit", "-c", "cses", "-t", "13", "-l", "C++", "-o", "C++17", "13.rs",
+        ])
+        .assert()
+        .success();
+
+    command()
+        .args(&["submit", "-c", "cses", "-t", "13", "-o", "C++17", "13.rs"])
+        .assert()
+        .failure()
+        .stdout(contains(r"Failed submitting file"));
+
+    command()
+        .args(&[
+            "submit", "-c", "cses", "-t", "13", "-l", "C++", "-o", "C++17", "13.rs",
+        ])
+        .assert()
+        .success();
+
+    command()
+        .args(&["submit", "-c", "cses", "-t", "13", "-l", "C++", "13.rs"])
+        .assert()
+        .failure()
+        .stdout(contains(r"Failed submitting file"));
 }
 
 #[distributed_slice(TESTS)]
@@ -131,7 +162,7 @@ fn compiler_report_is_dispayed_with_compile_error() {
         .assert();
     assert
         .success()
-        .stdout(regex_match(r"COMPILE ERROR"))
+        .stdout(contains(r"COMPILE ERROR"))
         .stdout(regex_match(r"(?i)compiler"))
         .stderr(predicate::str::is_empty());
 }
@@ -164,7 +195,7 @@ fn compiler_report_is_dispayed_with_compiler_warnings() {
         .assert();
     assert
         .success()
-        .stdout(regex_match(r"READY"))
+        .stdout(contains(r"READY"))
         .stdout(regex_match(r"(?i)compiler"))
         .stderr(predicate::str::is_empty());
 }
@@ -180,7 +211,61 @@ fn null_test_time_finishes_and_is_printed_correctly() {
         .assert();
     assert
         .success()
-        .stdout(regex_match(r"--"))
-        .stdout(regex_match(r"Result"))
+        .stdout(contains(r"--"))
+        .stdout(contains(r"Result"))
         .stderr(predicate::str::is_empty());
+}
+#[distributed_slice(TESTS)]
+fn null_test_time_finishes_and_is_print() {
+    log_in("kalle");
+    create_file("main.cpp", MAIN_CPP_CONTENT);
+
+    let assert = command()
+        .args(&[
+            "submit", "main.cpp", "-c", "progress", "-t", "7", "-l", "C++", "-o", "C++17",
+        ])
+        .assert();
+    assert
+        .success()
+        .stdout(contains(r"--"))
+        .stdout(contains(r"Result"))
+        .stderr(predicate::str::is_empty());
+}
+#[distributed_slice(TESTS)]
+fn submission_works_without_language_and_option() {
+    log_in("uolevi");
+
+    create_file("main.cpp", MAIN_CPP_CONTENT);
+    command()
+        .args(&["submit", "-c", "cses", "-t", "444", "main.cpp"])
+        .assert()
+        .success()
+        .stdout(contains(r"Result: WRONG ANSWER"))
+        .stdout(contains(r"Language: C++ (C++17)"));
+}
+#[distributed_slice(TESTS)]
+fn submission_works_without_language_with_option() {
+    log_in("uolevi");
+
+    create_file("main.cpp", MAIN_CPP_CONTENT);
+    command()
+        .args(&[
+            "submit", "-c", "cses", "-t", "555", "-o", "C++17", "main.cpp",
+        ])
+        .assert()
+        .success()
+        .stdout(contains(r"Result: WRONG ANSWER"))
+        .stdout(contains(r"Language: C++ (C++17)"));
+}
+#[distributed_slice(TESTS)]
+fn test_server_returns_null_language() {
+    log_in("uolevi");
+
+    create_file("main.asdf", MAIN_CPP_CONTENT);
+    command()
+        .args(&["submit", "-c", "cses", "-t", "111", "main.asdf"])
+        .assert()
+        .success()
+        .stdout(contains(r"Result: INVALID LANGUAGE"))
+        .stdout(contains(r"Language: ?"));
 }
