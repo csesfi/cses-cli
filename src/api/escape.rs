9c668d8182989839c12d17ad17a0949479154792
Kalle Luopajärvi <kalle.luopajarvi@helsinki.fi>
2021-06-24 16:18:43 +0300

    Add unit test for String::from(Escape)

--- src/api/escape.rs
+++ src/api/escape.rs
@@ -31,4 +31,10 @@ mod tests {
         let result = Escape("aBc123 +-ä").to_string();
         assert_eq!(result, "aBc123%20%2B-%C3%A4");
     }
+
+    #[test]
+    fn from_escape_to_string() {
+        let result: String = String::from(Escape("aBc123 +-ä"));
+        assert_eq!(result, "aBc123%20%2B-%C3%A4");
+    }
 }
